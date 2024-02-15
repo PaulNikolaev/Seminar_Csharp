545388c20fd9851194b41cdf24a71337bf1fbbc2
Poul Nikolaev <panikolaev7@gmail.com>
2024-02-15 13:28:33 +0300

    Seminar_1: Task_1 accomplished

--- Seminar_1/Task_1/Program.cs
+++ Seminar_1/Task_1/Program.cs
@@ -1,2 +1,12 @@
-﻿int a = 5;
-Console.WriteLine(a);
+﻿int firstNumber = 21;
+int secondNumber = 21;
+
+if (firstNumber == secondNumber) {
+    Console.WriteLine("числа равны");
+} 
+else if (firstNumber > secondNumber) {
+    Console.WriteLine("первое число больше");
+}
+    else {
+    Console.WriteLine("второе число больше");
+    }
