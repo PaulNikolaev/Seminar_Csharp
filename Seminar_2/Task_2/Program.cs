eac8e208aa6b916262b447d8e38fdbb4ea3f555d
Poul Nikolaev <panikolaev7@gmail.com>
2024-02-20 09:58:06 +0300

    Seminar_2: Task_2 accomplished 2

--- Seminar_2/Task_2/Program.cs
+++ Seminar_2/Task_2/Program.cs
@@ -9,12 +9,15 @@ int y = Convert.ToInt32(Console.ReadLine());
 if (x > 0 && y > 0){
     Console.WriteLine("1");
 }
-if (x < 0 && y > 0){
+else if (x < 0 && y > 0){
     Console.WriteLine("2");
 }
-if (x < 0 && y < 0){
+else if (x < 0 && y < 0){
     Console.WriteLine("3");
 }
-if (x > 0 && y < 0){
+else if (x > 0 && y < 0){
     Console.WriteLine("4");
+}
+else {
+    Console.WriteLine("Точка находиться на оси координат");
 }
