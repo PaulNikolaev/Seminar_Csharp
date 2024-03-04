9439b4760c8e23b45aca928770ef37fe61f0788e
Poul Nikolaev <panikolaev7@gmail.com>
2024-03-04 16:44:25 +0300

    add Seminar_6: Task_5

---
+++ Seminar_6/Task_5/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+Console.Clear();
+
+bool PalindromeCheck(char [] array) 
+{
+    bool result = true;
+    int i = 0;
+    while (result == true && i < array.Length / 2)
+    {
+        if (array[i] == array[array.Length - 1 - i])
+        {result = true;}
+        else
+        {result = false;}
+        i++;
+    }
+    return result;
+}
+
+Console.WriteLine("Задайте произвольную строку, чтобы проверить \n является ли она палиндромом:");
+string str = Console.ReadLine();
+
+char [] array = new char[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+    array[i] = str[i];
+}
+
+
+if (PalindromeCheck(array))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
