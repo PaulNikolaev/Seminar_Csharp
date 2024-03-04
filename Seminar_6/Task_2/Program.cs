ec1bf32a2359244bf945bc0634bdd06809af84c7
Poul Nikolaev <panikolaev7@gmail.com>
2024-03-04 12:11:57 +0300

    add Seminar_6: Task_2

---
+++ Seminar_6/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// На основе символов строки (тип string) сформировать массив символов (тип char[]). 
+// Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+Console.Clear();
+
+string str1 = "Hello";
+string str2 = "";
+char [] array = new char[str1.Length];
+for (int i = 0; i < str1.Length; i++)
+{
+    array[i] = str1[i];
+    // Console.Write($"{array[i]} \t");
+}
+
+foreach(char e in array)
+{
+    Console.Write(e + "\t");
+}
+
+
